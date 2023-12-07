4f351b492d5122ed113080f8e1c475378f73b45d
claire <yue.katou@gmail.com>
2023-12-07 15:02:15 -0800

    adds tag recovery logo header

---
+++ src/components/Header.svelte
@@ -0,0 +1,14 @@
+<script>
+  import Logo from "./Logo.svelte";
+</script>
+
+<header>
+  <Logo />
+</header>
+
+<style>
+  header {
+    width: 100%;
+    margin: 0 auto;
+  }
+</style>
---
+++ src/components/Logo.svelte
@@ -0,0 +1,11 @@
+<script>
+  import whiteLogo from "./../assets/main-logo-white-transparent.svg";
+</script>
+
+<img id="white-logo" src={whiteLogo} alt="Tag Recovery Logo" />
+
+<style>
+  #white-logo {
+    width: 320px;
+  }
+</style>
