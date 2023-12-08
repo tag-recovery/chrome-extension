3bfd1fd8a98a106a8f961483412a9dc8af4e556e
claire <yue.katou@gmail.com>
2023-12-08 14:09:53 -0800

    refactprs store data without loading storage immediately

--- src/scripts/store.js
+++ src/scripts/store.js
@@ -1,24 +1,22 @@
-import {
-  grabElements,
-  loadStorage,
-  injectRecordEventListeners,
-} from "./iconHelpers";
+import { grabElements, injectRecordEventListeners } from "./iconHelpers";
 import { recordsHtml } from "./html";
 
 const elements = grabElements();
 const { username } = elements;
 
-const store = { data: loadStorage(username), expDays: 5 };
+const store = { data: [], expDays: undefined };
 const storeProxy = new Proxy(store, {
   get(obj, prop) {
     return obj[prop];
   },
   set(obj, prop, value) {
-    localStorage.setItem(username, JSON.stringify(value));
     obj[prop] = value;
-    const recordsNode = document.getElementById("records-wrap");
-    recordsNode.innerHTML = recordsHtml(value);
-    injectRecordEventListeners({ ...elements, value: value });
+    if (prop === "data") {
+      localStorage.setItem(username, JSON.stringify(value));
+      const recordsNode = document.getElementById("records-wrap");
+      recordsNode.innerHTML = recordsHtml(value);
+      injectRecordEventListeners({ ...elements, value: value });
+    }
     return true;
   },
 });
