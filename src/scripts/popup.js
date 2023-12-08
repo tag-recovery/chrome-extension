0cd6977b9ebe40d2e7bff75aa906e930f1c663e9
claire <yue.katou@gmail.com>
2023-12-08 14:06:56 -0800

    changes DOMContentLoaded to conditional readyState check

--- src/scripts/popup.js
+++ src/scripts/popup.js
@@ -1,3 +1,11 @@
 import { registerPopup } from "./popupContent";
 
-document.addEventListener("DOMContentLoaded", registerPopup);
+if (document.readyState !== "loading") {
+  chrome.storage.local.get(["expDays"]).then((result) => {
+    if (result.expDays) {
+      registerPopup(result.expDays);
+    } else {
+      chrome.storage.local.set({ expDays: 3 }).then((res) => registerPopup(3));
+    }
+  });
+}
