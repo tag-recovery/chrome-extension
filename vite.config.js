c7e352d211430e5f37b6fbfafa4f43ca8a016e5a
claire <yue.katou@gmail.com>
2024-11-09 18:03:57 -0800

    adds functionality to falsify use_dynamic_url property

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,44 @@
 import { crx } from "@crxjs/vite-plugin";
 import { defineConfig } from "vite";
 import manifest from "./manifest.json";
+import vitePluginRunCommandOnDemand from "./vite-plugins";
+import { exec } from "child_process";
+import fs from "fs";
+import { createRequire } from "module";
+
+function forceDisableUseDynamicUrl(done) {
+  const require = createRequire(import.meta.url);
+  const manifest = require("./manifest.json");
+  manifest.web_accessible_resources.forEach((resource) => {
+    delete resource.use_dynamic_url;
+  });
+  if (!fs.existsSync("./manifest.json")) {
+    return done();
+  }
+  fs.writeFileSync("./manifest.json", JSON.stringify(manifest, null, 2));
+  done();
+}
+
+const runCommand = (command) => {
+  return new Promise((resolve, reject) => {
+    exec(command, (error, stdout, stderr) => {
+      if (error) {
+        console.error(`Error executing command: ${stderr}`);
+        reject(error);
+      } else {
+        console.log(stdout);
+        resolve();
+      }
+    });
+  });
+};
 
 export default defineConfig({
-  plugins: [crx({ manifest })],
+  plugins: [
+    crx({ manifest }),
+    vitePluginRunCommandOnDemand({
+      afterServerStart: () => runCommand("pnpm run forceDisableUseDynamicUrl"),
+      closeBundle: () => runCommand("pnpm run forceDisableUseDynamicUrl"),
+    }),
+  ],
 });
